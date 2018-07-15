c5db2c8def666a95caa030d00c43425faf5c1473
Muhammad Farag <muhammad.farag@shopify.com>
2018-07-14 22:50:50 -0400

    Pull struct stuff to its own file

--- HelloGo.go
+++ HelloGo.go
@@ -2,10 +2,7 @@ package main
 
 import "fmt"
 
-type person struct {
-	name string
-	age  int
-}
+
 
 func main() {
 
@@ -13,30 +10,7 @@ func main() {
 
 	fmt.Println(helloWorld)
 
-	a := person{
-		name: "Some name",
-		age:  10,
-	}
 
-	a1 := person {}
-	fmt.Println(a1)
-
-	a2 := person {"name 2", 2}
-	fmt.Println(a2)
-
-	a3 := new(person)
-	fmt.Println(a3)
-
-	fmt.Println(a)
-
-	b := struct {
-		description string
-	}{
-		description: "This is an anonymous struct!",
-	}
-
-	fmt.Println(b.description)
-	fmt.Println((&b).description)
 
 	c := 5
 	fmt.Println("the address of c is:", &c)
---
+++ Struct.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+	age  int
+}
+
+
+func main() {
+	a := person{
+		name: "Some name",
+		age:  10,
+	}
+
+	a1 := person {}
+	fmt.Println(a1)
+
+	a2 := person {"name 2", 2}
+	fmt.Println(a2)
+
+	a3 := new(person)
+	fmt.Println(a3)
+
+	fmt.Println(a)
+
+	b := struct {
+		description string
+	}{
+		description: "This is an anonymous struct!",
+	}
+
+	fmt.Println(b.description)
+	fmt.Println((&b).description)
+}
